9e3d18054f0a9f6e7f98d6e88d888370dc17f4c4
oswaldoJimenez <71518421+oswaldoJimenez@users.noreply.github.com>
2020-11-08 09:58:33 -0600

    Oswaldo

---
+++ app/src/main/java/com/example/OswAme/appnotas/Datos/DBOpenHelper.java
@@ -0,0 +1,102 @@
+    package com.example.OswAme.appnotas.Datos;
+
+    import android.content.Context;
+    import android.database.sqlite.SQLiteDatabase;
+    import android.database.sqlite.SQLiteOpenHelper;
+
+
+    public class DBOpenHelper extends SQLiteOpenHelper{
+
+        //CONSTRUCCION DE LA BD;
+        private static final String DB_NAME = "db_notes";
+        private static final int DB_VERSION = 2;
+
+
+
+        public static final String[]COLUMNS_REGISTROS = {"_id","tipo","titulo","descripcion","fecha_creacion","fecha_limite","hora_limite","checalo"};
+        public static final String TABLE_REGISTROS_NAME="registros";
+
+        private  final String TABLE_REGISTROS = "create table registros ("+
+                COLUMNS_REGISTROS[0]+" integer primary key autoincrement, "+
+                COLUMNS_REGISTROS[1]+" integer not null," +
+                COLUMNS_REGISTROS[2]+" varchar(100) not null,"+
+                COLUMNS_REGISTROS[3]+" text not null,"+
+                COLUMNS_REGISTROS[4]+" varchar(12) not null,"+
+                COLUMNS_REGISTROS[5]+" varchar(12) null,"+
+                COLUMNS_REGISTROS[6]+" varchar(12) null,"+
+                COLUMNS_REGISTROS[7]+" integer null);";
+
+
+
+        public static final String[]COLUMNS_ALERTAS = {"_id","id_Tarea","titulo","descripcion","fechaAlerta","horaAlerta"};
+        public static final String TABLE_ALERTAS_NAME="alertas";
+
+        private  final String TABLE_ALERTAS = "create table alertas ("+
+                COLUMNS_ALERTAS[0]+" integer primary key autoincrement, "+
+                COLUMNS_ALERTAS[1]+" integer not null," +
+                COLUMNS_ALERTAS[2]+" varchar(100) not null,"+
+                COLUMNS_ALERTAS[3]+" text not null,"+
+                COLUMNS_ALERTAS[4]+" varchar(12) null,"+
+                COLUMNS_ALERTAS[5]+" varchar(12) null);";
+
+
+
+        public static final String[]COLUMNS_MEDIA = {"_id","id_Tarea","dirUri","descripcion"};
+        public static final String TABLE_MEDIA_NAME="media";
+
+        private  final String TABLE_MEDIA = "create table media ("+
+                COLUMNS_MEDIA[0]+" integer primary key autoincrement, "+
+                COLUMNS_MEDIA[1]+" integer not null," +
+                COLUMNS_MEDIA[2]+" varchar not null,"+
+                COLUMNS_MEDIA[3]+" varchar(100) null);";
+
+
+
+
+
+
+
+
+
+
+
+        public DBOpenHelper(Context contexto) {
+
+            super(contexto, DB_NAME, null, DB_VERSION);
+
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+            try {
+
+                sqLiteDatabase.execSQL(TABLE_REGISTROS);
+                sqLiteDatabase.execSQL(TABLE_ALERTAS);
+                sqLiteDatabase.execSQL(TABLE_MEDIA);
+
+            }catch (Exception err){}
+
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+            try{
+
+                sqLiteDatabase.execSQL("DROP TABLE IF EXISTS registros");
+                sqLiteDatabase.execSQL("DROP TABLE IF EXISTS alertas");
+                sqLiteDatabase.execSQL("DROP TABLE IF EXISTS media");
+                onCreate(sqLiteDatabase);
+
+            }catch (Exception err){}
+
+        }
+
+
+
+
+
+
+
+    }
---
+++ app/src/main/java/com/example/OswAme/appnotas/Datos/DaoMedia.java
@@ -0,0 +1,152 @@
+package com.example.OswAme.appnotas.Datos;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DaoMedia {
+
+    private Context _contexto;
+    private SQLiteDatabase _midb;
+
+    //CREACION DE LA DB;
+    public DaoMedia(Context contexto){
+
+        this._contexto = contexto;
+        this._midb = new NotasBD(contexto).getWritableDatabase();
+
+    }
+
+    //INSERTAR DATOS;
+    public long insert(Media c){
+
+        ContentValues cv = new ContentValues();
+
+        cv.put(NotasBD.COLUMNS_MEDIA[1],c.getId_TareaNota());
+        cv.put(NotasBD.COLUMNS_MEDIA[2],c.getDir_uri());
+        cv.put(NotasBD.COLUMNS_MEDIA[3],c.getDescripMedia());
+
+        return _midb.insert(NotasBD.TABLE_MEDIA_NAME,null,cv) ;
+
+    }
+
+    //ACTUALIZAR DATOS;
+    public long update(Media c){
+
+        ContentValues cv = new ContentValues();
+
+        cv.put(NotasBD.COLUMNS_MEDIA[1],c.getId_TareaNota());
+        cv.put(NotasBD.COLUMNS_MEDIA[2],c.getDir_uri());
+        cv.put(NotasBD.COLUMNS_MEDIA[3],c.getDescripMedia());
+
+        return _midb.update(NotasBD.TABLE_MEDIA_NAME, cv, "_id=?", new String[] { String.valueOf( c.getId_media())});
+
+    }
+
+    //ELIMINAR DATOS (ID);
+    public int delete(String id){
+
+        return  _midb.delete("media","_id='"+id+"'",null);
+
+    }
+
+    //ELIMINAR DATOS (ID TAREA);
+    public int deleteTODAS(String id){
+
+        return  _midb.delete("media","id_Tarea='"+id+"'",null);
+
+    }
+
+    //CREACION DE LISTA;
+    public List<Media> buscarTodos() {
+
+        List<Media> notesArrayList = new ArrayList<Media>();
+        String selectQuery = "SELECT * FROM media";
+        Log.d("", selectQuery);
+        SQLiteDatabase db = this._midb;
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        if (c.moveToFirst()) {
+
+            do {
+
+                Media notas = new Media();
+                notas.setId_media(c.getInt(c.getColumnIndex("_id")));
+                notas.setId_TareaNota(c.getInt(c.getColumnIndex("id_Tarea")));
+                notas.setDir_uri(c.getString(c.getColumnIndex("dirUri")));
+                notas.setDescripMedia(c.getString(c.getColumnIndex("descripcion")));
+
+                notesArrayList.add(notas);
+
+            } while (c.moveToNext());
+
+        }
+
+        return notesArrayList;
+
+    }
+
+    //BUSQUEDA POR ID TAREA;
+    public List<Media> buscarTodosDeTarea(int iden) {
+
+        List<Media> notesArrayList = new ArrayList<Media>();
+        String selectQuery = "SELECT * FROM media WHERE id_Tarea = '"+iden+"'";
+        Log.d("", selectQuery);
+        SQLiteDatabase db = this._midb;
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        if (c.moveToFirst()) {
+
+            do {
+
+                Media notas = new Media();
+                notas.setId_media(c.getInt(c.getColumnIndex("_id")));
+                notas.setId_TareaNota(c.getInt(c.getColumnIndex("id_Tarea")));
+                notas.setDir_uri(c.getString(c.getColumnIndex("dirUri")));
+                notas.setDescripMedia(c.getString(c.getColumnIndex("descripcion")));
+
+                notesArrayList.add(notas);
+
+            } while (c.moveToNext());
+
+        }
+
+        return notesArrayList;
+
+    }
+
+    //BUSQUEDA POR ID;
+    public Media buscarUno(int iden) {
+
+        Media notesUno = new Media();
+        String selectQuery = "SELECT * FROM media WHERE _id = '"+iden+"'";
+        Log.d("", selectQuery);
+        SQLiteDatabase db = this._midb;
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        if (c.moveToFirst()) {
+
+            do {
+
+                Media notas = new Media();
+                notas.setId_media(c.getInt(c.getColumnIndex("_id")));
+                notas.setId_TareaNota(c.getInt(c.getColumnIndex("id_Tarea")));
+                notas.setDir_uri(c.getString(c.getColumnIndex("dirUri")));
+                notas.setDescripMedia(c.getString(c.getColumnIndex("descripcion")));
+
+                notesUno = notas;
+
+            } while (c.moveToNext());
+
+        }
+
+        return notesUno;
+
+    }
+}
