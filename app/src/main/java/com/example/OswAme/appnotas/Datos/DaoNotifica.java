2555144e6ba36778ea4c67a7dc292e3a0f3c29fc
oswaldoJimenez <71518421+oswaldoJimenez@users.noreply.github.com>
2020-11-29 17:01:02 -0600

    Video

---
+++ app/src/main/java/com/example/OswAme/appnotas/Datos/DaoNotifica.java
@@ -0,0 +1,206 @@
+package com.example.OswAme.appnotas.Datos;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class DaoNotifica {
+
+    private Context _contexto;
+    private SQLiteDatabase _midb;
+
+
+    //CREACION DE LA DB;
+    public DaoNotifica(Context contexto){
+
+        this._contexto = contexto;
+        this._midb = new NotasBD(contexto).getWritableDatabase();
+
+    }
+
+    //INSERTAR DATOS;
+    public long insert(Alerta c){
+
+        ContentValues cv = new ContentValues();
+
+        cv.put(NotasBD.COLUMNS_NOTIFICACIONES[1],c.getId_tarea());
+        cv.put(NotasBD.COLUMNS_NOTIFICACIONES[2],c.getTituloAlerta());
+        cv.put(NotasBD.COLUMNS_NOTIFICACIONES[3],c.getDescripcionAlerta());
+        cv.put(NotasBD.COLUMNS_NOTIFICACIONES[4],c.getFechaAlerta());
+        cv.put(NotasBD.COLUMNS_NOTIFICACIONES[5],c.getHoraAlerta());
+
+        return _midb.insert(NotasBD.TABLE_NOTIFICACIONES_NAME,null,cv) ;
+
+    }
+
+    //ACTUALIZAR DATOS;
+    public long update(Alerta c){
+
+        ContentValues cv = new ContentValues();
+
+        cv.put(NotasBD.COLUMNS_NOTIFICACIONES[1],c.getId_tarea());
+        cv.put(NotasBD.COLUMNS_NOTIFICACIONES[2],c.getTituloAlerta());
+        cv.put(NotasBD.COLUMNS_NOTIFICACIONES[3],c.getDescripcionAlerta());
+        cv.put(NotasBD.COLUMNS_NOTIFICACIONES[4],c.getFechaAlerta());
+        cv.put(NotasBD.COLUMNS_NOTIFICACIONES[5],c.getHoraAlerta());
+
+        return _midb.update(NotasBD.TABLE_NOTIFICACIONES_NAME, cv, "_id=?", new String[] { String.valueOf( c.getId_alerta())});
+
+    }
+
+    //ELIMINAR DATOS (ID);
+    public int delete(String id){
+
+        return  _midb.delete("alertas","_id='"+id+"'",null);
+
+    }
+
+    //ELIMINAR DATOS (ID TAREA);
+    public int deleteTODAS(String id){
+
+        return  _midb.delete("alertas","id_Tarea='"+id+"'",null);
+
+    }
+
+    //CREACION DE LISTA;
+    public List<Alerta> buscarTodos() {
+
+        List<Alerta> notesArrayList = new ArrayList<Alerta>();
+        String selectQuery = "SELECT * FROM alertas";
+        Log.d("", selectQuery);
+        SQLiteDatabase db = this._midb;
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        if (c.moveToFirst()) {
+
+            do {
+
+                Alerta notas = new Alerta();
+                notas.setId_alerta(c.getInt(c.getColumnIndex("_id")));
+                notas.setId_tarea(c.getInt(c.getColumnIndex("id_Tarea")));
+                notas.setTituloAlerta(c.getString(c.getColumnIndex("titulo")));
+                notas.setDescripcionAlerta(c.getString(c.getColumnIndex("descripcion")));
+                notas.setFechaAlerta(c.getString(c.getColumnIndex("fechaAlerta")));
+                notas.setHoraAlerta(c.getString(c.getColumnIndex("horaAlerta")));
+
+                notesArrayList.add(notas);
+
+            } while (c.moveToNext());
+
+        }
+
+        return notesArrayList;
+
+    }
+
+    //BUSQUEDA POR ID TAREA;
+    public List<Alerta> buscarTodosDeTarea(int iden) {
+
+        List<Alerta> notesArrayList = new ArrayList<Alerta>();
+        String selectQuery = "SELECT * FROM alertas WHERE id_Tarea = '"+iden+"'";
+        Log.d("", selectQuery);
+        SQLiteDatabase db = this._midb;
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        if (c.moveToFirst()) {
+
+            do {
+
+                Alerta notas = new Alerta();
+                notas.setId_alerta(c.getInt(c.getColumnIndex("_id")));
+                notas.setId_tarea(c.getInt(c.getColumnIndex("id_Tarea")));
+                notas.setTituloAlerta(c.getString(c.getColumnIndex("titulo")));
+                notas.setDescripcionAlerta(c.getString(c.getColumnIndex("descripcion")));
+                notas.setFechaAlerta(c.getString(c.getColumnIndex("fechaAlerta")));
+                notas.setHoraAlerta(c.getString(c.getColumnIndex("horaAlerta")));
+
+                notesArrayList.add(notas);
+
+            } while (c.moveToNext());
+
+        }
+
+        return notesArrayList;
+
+    }
+
+    //BUSQUEDA POR FECHA;
+    public List<Alerta> buscarTodosDeFecha(String iden) {
+
+        List<Alerta> notesArrayList = new ArrayList<Alerta>();
+        String selectQuery = "SELECT * FROM alertas WHERE fechaAlerta = '"+iden+"'";
+        Log.d("", selectQuery);
+        SQLiteDatabase db = this._midb;
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        if (c.moveToFirst()) {
+
+            do {
+
+                Alerta notas = new Alerta();
+                notas.setId_alerta(c.getInt(c.getColumnIndex("_id")));
+                notas.setId_tarea(c.getInt(c.getColumnIndex("id_Tarea")));
+                notas.setTituloAlerta(c.getString(c.getColumnIndex("titulo")));
+                notas.setDescripcionAlerta(c.getString(c.getColumnIndex("descripcion")));
+                notas.setFechaAlerta(c.getString(c.getColumnIndex("fechaAlerta")));
+                notas.setHoraAlerta(c.getString(c.getColumnIndex("horaAlerta")));
+
+                notesArrayList.add(notas);
+
+            } while (c.moveToNext());
+
+        }
+
+        return notesArrayList;
+
+    }
+
+    //BUSQUEDA POR ID;
+    public Alerta buscarUno(int iden) {
+
+        Alerta notesUno = new Alerta();
+        String selectQuery = "SELECT * FROM alertas WHERE _id = '"+iden+"'";
+        Log.d("", selectQuery);
+        SQLiteDatabase db = this._midb;
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        if (c.moveToFirst()) {
+
+            do {
+
+                Alerta notas = new Alerta();
+                notas.setId_alerta(c.getInt(c.getColumnIndex("_id")));
+                notas.setId_tarea(c.getInt(c.getColumnIndex("id_Tarea")));
+                notas.setTituloAlerta(c.getString(c.getColumnIndex("titulo")));
+                notas.setDescripcionAlerta(c.getString(c.getColumnIndex("descripcion")));
+                notas.setFechaAlerta(c.getString(c.getColumnIndex("fechaAlerta")));
+                notas.setHoraAlerta(c.getString(c.getColumnIndex("horaAlerta")));
+
+                notesUno = notas;
+
+            } while (c.moveToNext());
+
+        }
+
+        return notesUno;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/OswAme/appnotas/Media/Servicio.java
@@ -0,0 +1,165 @@
+package com.example.OswAme.appnotas.Media;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v7.app.NotificationCompat;
+
+import com.example.OswAme.appnotas.Datos.Alerta;
+import com.example.OswAme.appnotas.Datos.DaoNotas;
+import com.example.OswAme.appnotas.Datos.DaoNotifica;
+import com.example.OswAme.appnotas.Datos.Nota_Serial;
+import com.example.OswAme.appnotas.MainActivity;
+import com.example.OswAme.appnotas.R;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+
+
+public class Servicio extends Service {
+
+    @Override
+    public void onCreate(){}
+
+    hilo h;
+    @Override
+    public int onStartCommand(Intent intent,int flag,int idProcess){
+
+        try{
+
+            if(h==null) {
+                h = new hilo();
+                h.start();
+            }
+
+        }catch (Exception err){}
+
+        return START_STICKY;
+
+    }
+
+    @Override
+    public void onDestroy(){
+
+        try{
+
+            if(h.isAlive()) {
+                h.stop();
+            }
+
+        }catch (Exception err){}
+
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    private ArrayList<Alerta> listarecordatorios = new ArrayList<>() ;
+    int aux=0;
+    public void btnNoti_click(String Titulo,String Descripcion,int indice) {
+
+        aux=aux+1;
+        NotificationCompat.Builder mBuilder;
+        NotificationManager mNotifyMgr = (NotificationManager) getApplicationContext().getSystemService(NOTIFICATION_SERVICE);
+
+        Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+
+        int icono = R.mipmap.ic_launcher;
+
+        long hora = System.currentTimeMillis();
+
+        Intent i = new Intent(this, MainActivity.class);
+
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, 0);
+
+        mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(getApplicationContext())
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(icono)
+                .setContentTitle(Titulo)
+                .setContentText(Descripcion)
+                .setWhen(hora)
+                .setVibrate(new long[]{100, 250, 100, 500})
+                .setAutoCancel(true)
+                .setSound(defaultSound);
+
+        mNotifyMgr.notify(aux, mBuilder.build());
+
+    }
+
+
+    public class hilo extends Thread{
+        @Override
+        public  void  run(){
+            while (true){
+
+                try {
+
+                    DaoNotifica dao = new DaoNotifica(getApplicationContext());
+                    DaoNotas daonotas = new DaoNotas(getApplicationContext());
+
+
+                    final Calendar c= Calendar.getInstance();
+                    String fecha = c.get(Calendar.DAY_OF_MONTH)+"-"+(c.get(Calendar.MONTH)+1)+"-"+c.get(Calendar.YEAR);
+                    String hora = c.get(Calendar.HOUR_OF_DAY)+":"+c.get(Calendar.MINUTE);
+
+
+                    List<Alerta> lista ;
+                    lista = dao.buscarTodosDeFecha(fecha);
+
+
+                    Alerta alertas = new Alerta();
+                    alertas.setId_alerta(1);
+                    alertas.setId_tarea(1);
+                    alertas.setTituloAlerta("Titulo");
+                    alertas.setDescripcionAlerta("Descripcion");
+                    alertas.setFechaAlerta("Fecha");
+                    alertas.setHoraAlerta("Hora");
+
+                    lista.add(alertas);
+
+
+                    for (int i = 0; i < lista.size(); i++) {
+                        if (lista.get(i).getHoraAlerta().equalsIgnoreCase(hora) && ((c.get(Calendar.SECOND)) == 0)) {
+
+                            Alerta obj = new Alerta();
+                            obj.setId_alerta(lista.get(i).getId_alerta());
+                            obj.setFechaAlerta(lista.get(i).getFechaAlerta());
+                            obj.setHoraAlerta(lista.get(i).getHoraAlerta());
+
+                            listarecordatorios.add(obj);
+
+                            Nota_Serial nota = new Nota_Serial();
+                            nota = daonotas.buscarUno(lista.get(i).getId_tarea());
+
+                            if(nota.getTitulo().trim().length()>0 && nota.getDescripcion().trim().length()>0) {
+                                btnNoti_click(nota.getTitulo(), nota.getDescripcion(), i);
+                            }
+
+                        }
+
+                    }
+
+                    Thread.sleep(1000);
+
+                } catch (Exception e) {}
+
+            }
+
+        }
+
+    }
+
+}
